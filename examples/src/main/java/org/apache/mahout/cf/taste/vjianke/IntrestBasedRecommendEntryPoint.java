a2b08acfcd10cd80ce2a41f080902f7e7eef62d0
xiaol <liuxiao.in@gmail.com>
2013-05-07 16:26:15 +0800

    update baseline

--- examples/src/main/java/org/apache/mahout/cf/taste/vjianke/IntrestBasedRecommendEntryPoint.java
+++ examples/src/main/java/org/apache/mahout/cf/taste/vjianke/IntrestBasedRecommendEntryPoint.java
@@ -115,15 +115,15 @@ public class IntrestBasedRecommendEntryPoint {
         Hashtable<String, Datalayer.UserEntity> userEntities = datalayer.QueryUsers();
         //JSONArray activeUsers = datalayer.getActiveUsers(15);
         //for(Object actvieUser:activeUsers){
-        //for(Map.Entry<String, Datalayer.UserEntity> userEntity:userEntities.entrySet()){
-        for(String mate:mates){
-            //String userId = userEntity.getKey();
+        for(Map.Entry<String, Datalayer.UserEntity> userEntity:userEntities.entrySet()){
+        //for(String mate:mates){
+            String userId = userEntity.getKey();
             //String userId = IntrestBasedRecommendEntryPoint.mates.get(18).toUpperCase();
             //StringBuilder sb = new StringBuilder((String)actvieUser);
             //sb.insert(8,"-").insert(13,"-").insert(18,"-").insert(23,"-");
             //String userId = UUID.fromString(sb.toString()).toString().toUpperCase();
             //String userId = "07221718-B190-4536-8191-A0410029DE34";
-            String userId = mate.toUpperCase();
+            //String userId = mate.toUpperCase();
             List<String> boards = datalayer.querySubscription(userId);
             count++;
             //List<Datalayer.BoardRelated> relatedBoards = datalayer.queryRelatedBoards(uuid);
